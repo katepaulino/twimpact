468696ffc53859fc280a9115792ecb242848f889
Kate Lucas <katherinelucas@gmail.com>
2016-02-11 23:26:39 -0800

    first commit for twimpact

---
+++ script.js
@@ -0,0 +1,4 @@
+// function to find representative after 'find' button is clicked
+// displays representative and shows the 5 issues
+// "Your representative is [name] and will be sending a tweet about [issue]"
+// after [issue] is clicked, twitter form popluates automated message about that issue
