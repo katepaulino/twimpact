22a6ee2accc9bebc421176182c05cf3cb687e7a0
Kate Lucas <katherinelucas@gmail.com>
2016-02-15 11:41:35 -0800

    added more images, worked on html structure, got some jquery functions in there

--- js/app.js
+++ js/app.js
@@ -1,5 +1,59 @@
+window.onload = function() {
+
+$('#div_1').hide();
+$('#div_2').hide();
+$('#div_3').hide();
+$('#div_4').hide();
+$('#div_5').hide();
+
 $(document).ready(function(){
-    $("button").click(function(){
-        $("p").toggle();
+    $('#reach').click(function(){
+        $('#div_1').show();
+        $('#div_2').hide();
+        $('#div_3').hide();
+        $('#div_4').hide();
+        $('#div_5').hide();
     });
 });
+
+$(document).ready(function(){
+    $('#globalFood').click(function(){
+      $('#div_1').hide();
+      $('#div_2').show();
+      $('#div_3').hide();
+      $('#div_4').hide();
+      $('#div_5').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#transparency').click(function(){
+      $('#div_1').hide();
+      $('#div_2').hide();
+      $('#div_3').show();
+      $('#div_4').hide();
+      $('#div_5').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#reform').click(function(){
+      $('#div_1').hide();
+      $('#div_2').hide();
+      $('#div_3').hide();
+      $('#div_4').show();
+      $('#div_5').hide();
+    });
+});
+
+$(document).ready(function(){
+    $('#core').click(function(){
+      $('#div_1').hide();
+      $('#div_2').hide();
+      $('#div_3').hide();
+      $('#div_4').hide();
+      $('#div_5').show();
+    });
+});
+
+};
